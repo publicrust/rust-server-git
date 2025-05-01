b2a17ab313550d26a137a6e80bf83efb540c98c1
skulidropek <legovlad818@gmail.com>
2025-05-01 12:47:41 +0400

    Add rust server code and upload-artifact action

---
+++ .github/actions/upload-artifact/src/upload-inputs.ts
@@ -0,0 +1,28 @@
+import {NoFileOptions} from './constants'
+
+export interface UploadInputs {
+  /**
+   * The name of the artifact that will be uploaded
+   */
+  artifactName: string
+
+  /**
+   * The search path used to describe what to upload as part of the artifact
+   */
+  searchPath: string
+
+  /**
+   * The desired behavior if no files are found with the provided search path
+   */
+  ifNoFilesFound: NoFileOptions
+
+  /**
+   * Duration after which artifact will expire in days
+   */
+  retentionDays: number
+
+  /**
+   * Whether or not to include hidden files in the artifact
+   */
+  includeHiddenFiles: boolean
+}
